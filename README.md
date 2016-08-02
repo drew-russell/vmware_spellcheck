35297bc377c81b191e0d1b4f209d74d28740fa98
Drew Russell <Drew.Russell@wwt.com>
2016-08-02 00:27:32 -0500

    initil sync

---
+++ README.md
@@ -0,0 +1,16 @@
+### VMware Spellcheck
+=================
+
+A Chrome extension that corrects the common misspellings of VMware.
+
+[Direct download of crx file](https://github.com/lahdekorpi/cloud-to-bullshit/raw/master/CloudToBullshit.crx?raw=true)
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag CloudToBullshit.crx into the page that appears.
+
+Original cloud-to-butt
+-------------------------
+
+This is a fork of https://github.com/panicsteve/cloud-to-butt
---
+++ Source/content_script.js
@@ -0,0 +1,40 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+
+	v = v.replace(/\Vmware\b/g, "VMware");
+	v = v.replace(/\VMWare\b/g, "VMware");
+	v = v.replace(/\bvmware\b/g, "VMware");
+
+	textNode.nodeValue = v;
+}
